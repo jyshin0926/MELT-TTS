9f8199e3fd35914a6792c7299f72ff36b4c148ec
jyshin0926 <sjy777star@gmail.com>
2024-09-06 09:21:30 +0000

    setting training code for mmtts image-text pair dataset

--- vits/mel_processing.py
+++ vits/mel_processing.py
@@ -141,8 +141,8 @@ def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size,
 
     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
 
-    # spec = torch.matmul(mel_basis[fmax_dtype_device], spec)
-    spec = torch.mul(mel_basis[fmax_dtype_device], spec)
+    spec = torch.matmul(mel_basis[fmax_dtype_device], spec)
+    # spec = torch.mul(mel_basis[fmax_dtype_device], spec)
     spec = spectral_normalize_torch(spec)
 
     return spec
